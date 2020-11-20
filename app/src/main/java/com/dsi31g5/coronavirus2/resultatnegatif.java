63c2ef454992288fdd9dadb3404c0e19c6ac393b
tarchounZaineb <zaynebtarchoun@gmail.com>
2020-11-21 00:54:19 +0100

    troisiéme commit:activity_resultatnegatif.xml /resultatnegatif.java

---
+++ app/src/main/java/com/dsi31g5/coronavirus2/resultatnegatif.java
@@ -0,0 +1,28 @@
+package com.hajer.coronavirus;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+import android.os.Bundle;
+
+import com.example.coronavirus.R;
+
+
+public class resultatnegatif extends AppCompatActivity {
+
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_resultatnegatif);
+
+    }
+
+}
